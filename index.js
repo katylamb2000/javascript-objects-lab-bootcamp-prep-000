fdbb80df2d183502f85317d663fe48fa42aee719
katy@karstaway.com <katy@karstaway.com>
2018-07-03 15:11:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function deleteFromObjectByKey() {
   return newRecipes
 }
 
-function destructivelyDeleteFromObjectByKey(recipe) {
-  delete recipes.key
+function destructivelyDeleteFromObjectByKey() {
+  delete recipes.key;
   return recipes
 }
